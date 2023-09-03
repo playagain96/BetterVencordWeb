fd2ab2c91c904e526d4721f42c6c9fb0371b0420
Daniel <davilareko@gmail.com>
2023-09-03 16:08:59 +0200

    Clean up

--- src/plugins/bdCompatLayer/fakeStuff.js
+++ src/plugins/bdCompatLayer/fakeStuff.js
@@ -47,22 +47,15 @@ export const addDiscordModules = () => {
             return BdApi.Webpack;
         }
     };
-    // context.WebpackModules = (function () {
-    //     return BdApi.Webpack;
-    // })();
     const ModuleDataText = this.simpleGET(
         proxyUrl +
         "https://github.com/BetterDiscord/BetterDiscord/raw/main/renderer/src/modules/discordmodules.js"
     ).responseText.replaceAll("\r", "");
-    // const ev = "(" + ModuleDataText.split("export default Utilities.memoizeObject(")[1].replaceAll("WebpackModules", "BdApi.Webpack");
     const ev =
         "(" +
         ModuleDataText.split("export default Utilities.memoizeObject(")[1];
     const sourceBlob = new Blob([ev], { type: "application/javascript" });
     const sourceBlobUrl = URL.createObjectURL(sourceBlob);
-    // return eval(ev + "\n//# sourceURL=" + sourceBlobUrl);
-    // console.log(evaled);
-    // windowBdCompatLayer.discordModulesBlobUrl = sourceBlobUrl;
     return { output: evalInScope(ev + "\n//# sourceURL=" + sourceBlobUrl, context), sourceBlobUrl };
 };
 
@@ -83,7 +76,6 @@ export const addContextMenu = DiscordModules => {
             return BdApi.Patcher;
         }
     };
-    // const ev = "(" + ModuleDataText.split("export default Utilities.memoizeObject(")[1].replaceAll("WebpackModules", "BdApi.Webpack");
     const linesToRemove = this.findFirstLineWithoutX(
         ModuleDataText,
         "import"
@@ -93,19 +85,6 @@ export const addContextMenu = DiscordModules => {
     ModuleDataArr.splice(0, linesToRemove);
     ModuleDataArr.pop();
     ModuleDataArr.pop();
-    // const Logger = {
-    //     warn: function (...args) {
-    //         console.warn(...args);
-    //     },
-    //     info: function (...args) {
-    //         console.log(...args);
-    //     },
-    //     err: function (...args) {
-    //         console.error(...args);
-    //     },
-    // };
-
-    // const ModuleDataAssembly = "(()=>{" + addLogger.toString() + ";const Logger = addLogger();const {React} = eval(`(" + this.objectToString(DiscordModules) + ")`);" + ModuleDataArr.join("\n") + "\nreturn ContextMenu;})();";
     const ModuleDataAssembly =
         "(()=>{" +
         addLogger.toString() +
@@ -116,11 +95,6 @@ export const addContextMenu = DiscordModules => {
         type: "application/javascript",
     });
     const sourceBlobUrl = URL.createObjectURL(sourceBlob);
-    // window.BdApi.ContextMenu = new (eval(
-    //     ModuleDataAssembly + "\n//# sourceURL=" + sourceBlobUrl
-    // ))();
     const evaluatedContextMenu = evalInScope(ModuleDataAssembly + "\n//# sourceURL=" + sourceBlobUrl, context);
-    // window.BdApi.ContextMenu = ;
-    // windowBdCompatLayer.contextMenuBlobUrl = sourceBlobUrl;
     return { output: new evaluatedContextMenu(), sourceBlobUrl };
 };
