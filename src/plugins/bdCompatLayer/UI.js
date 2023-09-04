dcb28c76e33fecd5a355a66fa658be388d3fbf4f
ImAFrogOwO <dev.zero06@gmail.com>
2023-09-04 16:39:51 -0500

    Changed "class" => "export function"

--- src/plugins/bdCompatLayer/UI.js
+++ src/plugins/bdCompatLayer/UI.js
@@ -17,48 +17,47 @@
 */
 
 // eslint-disable-next-line semi
-class UI {
-    helper() {
-        console.log("hi");
-    }
-    alert() {
-        // Redoing
-    }
-    toast() {
-        BdApi.DOM.addStyle(
-            "bv-toasts",
-            `
-            @keyframes bv-toast-up {
-                from {
-                    transform: translateY(0);
-                    opacity: 0;
-                }
-            }
-            
-            .some-toast-shit {
-                position: fixed;
-                bottom: 20px; 
-                left: 50%; 
-                transform: translateX(-50%) translateY(0);
-                animation: bv-toast-up 300ms ease;
-                background-color: var(--background-primary);
-                padding: 10px;
-                border-radius: 5px;
-                box-shadow: var(--elevation-medium), var(--elevation-stroke);
-                font-weight: 500;
-                color: var(--header-primary);
-                font-size: 14px;
-                opacity: 1;
-                pointer-events: none;
-                user-select: none;
-            }`
-        );
-
-        // Create a toast element
-        const toast = document.createElement("div");
-        toast.classList.add("some-toast-shit");
-        document.body.appendChild(toast);
-    }
+export function helper() {
+    console.log("hi");
 }
 
-export default UI;
+export function alert() {
+    // Redoing
+}
+
+export function toast() {
+    BdApi.DOM.addStyle(
+        "bv-toasts",
+        `
+        @keyframes bv-toast-up {
+          from {
+            transform: translateY(0);
+            opacity: 0;
+          }
+        }
+
+        .some-toast-shit {
+          position: fixed;
+          bottom: 20px;
+          left: 50%;
+          transform: translateX(-50%) translateY(0);
+          animation: bv-toast-up 300ms ease;
+          background-color: var(--background-primary);
+          padding: 10px;
+          border-radius: 5px;
+          box-shadow: var(--elevation-medium), var(--elevation-stroke);
+          font-weight: 500;
+          color: var(--header-primary);
+          font-size: 14px;
+          opacity: 1;
+          pointer-events: none;
+          user-select: none;
+        }
+      `
+    );
+
+    // Create a toast element
+    const toast = document.createElement("div");
+    toast.classList.add("some-toast-shit");
+    document.body.appendChild(toast);
+}
